e3046227eb4e88c605fb960a4cba05a8d129ec0d
hubert.lenoir <hubert.lenoir@sensiolabs.com>
2024-10-08 11:36:11 +0200

    [FeatureFlag] Use providers

---
+++ src/Symfony/Component/FeatureFlag/Provider/ChainProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\FeatureFlag\Provider;
+
+class ChainProvider implements ProviderInterface
+{
+    public function __construct(
+        private readonly array $providers = [],
+    ) {
+    }
+
+    public function has(string $featureName): bool
+    {
+        foreach ($this->providers as $provider) {
+            if ($provider->has($featureName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function get(string $featureName): \Closure
+    {
+        foreach ($this->providers as $provider) {
+            if ($provider->has($featureName)) {
+                return $provider->get($featureName);
+            }
+        }
+
+        return fn() => false;
+    }
+
+    public function getNames(): array
+    {
+        return array_unique(array_merge(...array_map(fn (ProviderInterface $provider) => $provider->getNames(), $this->providers)));
+    }
+}
